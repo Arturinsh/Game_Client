a1fc4d286efd7ce5d9aed6666846e5d3ecab3129
Arturs Ontuzans <arturs.ontuzans@gmail.com>
2015-11-15 02:28:11 +0200

    Added methods for register and player join testing

--- core/src/xyz/arturinsh/NetworkListener/NetworkListener.java
+++ core/src/xyz/arturinsh/NetworkListener/NetworkListener.java
@@ -4,8 +4,12 @@ import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 
 import xyz.arturinsh.GameWorld.GameWorld;
+import xyz.arturinsh.NetworkListener.Packets.AddPlayer;
 import xyz.arturinsh.NetworkListener.Packets.LogInFailed;
 import xyz.arturinsh.NetworkListener.Packets.LogInSuccess;
+import xyz.arturinsh.NetworkListener.Packets.RegisterFailed;
+import xyz.arturinsh.NetworkListener.Packets.RegisterSuccess;
+import xyz.arturinsh.NetworkListener.Packets.RemovePlayer;
 
 public class NetworkListener extends Listener {
 	private GameWorld world;
@@ -29,6 +33,20 @@ public class NetworkListener extends Listener {
 		}
 		if (object instanceof LogInFailed) {
 			world.showDialog("LogInFailed");
+		}
+		if (object instanceof RegisterSuccess) {
+			world.showDialog("Register Succes");
+		}
+		if (object instanceof RegisterFailed) {
+			world.showDialog("Register Failed");
+		}
+		if (object instanceof AddPlayer) {
+			AddPlayer player = (AddPlayer) object;
+			world.showDialog(player.username + " joinded");
+		}
+		if (object instanceof RemovePlayer) {
+			RemovePlayer player = (RemovePlayer) object;
+			world.showDialog(player.username + " left");
 		}
 	}
 }
